8c15df0f04cb99891b61a40611c9b2598402726d
VeniVidiGiti <99083579+VENIVIDIGITl@users.noreply.github.com>
2022-04-20 23:56:38 +0300

    add decimal button functionality to add decimal point to display value when clicked

--- script.js
+++ script.js
@@ -7,8 +7,16 @@ function sendNumberValue(number) {
   // if current display value is 0, replace it, if not, add number
   const displayValue = calculatorDisplay.textContent;
   calculatorDisplay.textContent = displayValue === '0' ? number : displayValue + number;
-  
 }
+
+
+function addDecimal() {
+  // if no decimal, add one
+  if (!calculatorDisplay.textContent.includes('.')) {
+    calculatorDisplay.textContent = `${calculatorDisplay.textContent}.`;
+  }
+}
+
 
 // Add Event Listeners for numbers, operators, decimal buttons
 inputBtns.forEach((inputBtn) => {
@@ -17,7 +25,7 @@ inputBtns.forEach((inputBtn) => {
   } else if (inputBtn.classList.contains('operator')) {
     inputBtn.addEventListener('click', () => sendNumberValue(inputBtn.value));
   } else if (inputBtn.classList.contains('decimal')) {
-    inputBtn.addEventListener('click', () => sendNumberValue(inputBtn.value));
+    inputBtn.addEventListener('click', () => addDecimal());
   } 
 });
 
